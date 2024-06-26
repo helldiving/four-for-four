9023b777c7b1377d9b240575eb588671e833afaa
helldiving <142443321+helldiving@users.noreply.github.com>
2024-06-26 11:53:50 -0400

    Test commit

---
+++ src/test/java/com/helldiving/websocket_application/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helldiving.websocket_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
